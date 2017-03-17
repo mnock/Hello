5b214a8cb2c15c8626e44fef03196357868c5e38
ck <690065587@qq.com>
2017-03-17 15:16:27 +0800

    Initial commit

---
+++ app/src/main/java/com/mno/hello/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mno.hello;
+
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"首次提交",Toast.LENGTH_LONG).show();
+    }
+}
